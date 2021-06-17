81c99b7792e6d286fd22baec6863d8b293bc79d7
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2021-06-17 15:13:38 +0200

    fix: panic when sleeping in portrait
    Former-commit-id: 1b47bc0bdae2bf4f1dca88f02468b7864b58007e

--- internal/client/waiting.go
+++ internal/client/waiting.go
@@ -20,45 +20,47 @@ func (g *Grabber) setWaitingPicture(ctx context.Context) error {
 	x := 50
 	y := 50
 	var img *image.Gray
-	if g.rot.orientation == portrait {
-		img = image.NewGray(image.Rect(0, 0, Height, Width))
-	} else {
-		img = image.NewGray(image.Rect(0, 0, Width, Height))
-	}
-	draw.Draw(img, img.Bounds(), image.Black, image.Point{}, draw.Src)
-	label := "Waiting for reMarkable server at " + g.conf.ServerAddr
-	fnt, err := truetype.Parse(goregular.TTF)
-	if err != nil {
-		return err
-	}
-	face := truetype.NewFace(fnt, &truetype.Options{
-		Size: 36,
-	})
-	// img is now an *image.RGBA
-	point := fixed.Point26_6{
-		X: fixed.Int26_6(x * 64),
-		Y: fixed.Int26_6(y * 64),
-	}
 
-	d := &font.Drawer{
-		Dst:  img,
-		Src:  image.NewUniform(color.White),
-		Face: face, //basicfont.Face7x13,
-		Dot:  point,
-	}
-	d.DrawString(label)
 	var run bool
 	tick := time.NewTicker(500 * time.Millisecond)
-	col := color.White
-	d.Face = truetype.NewFace(fnt, &truetype.Options{
-		Size: 64,
-	})
-
+	var d *font.Drawer
+	var col color.Color
 	for {
 		select {
 		case <-ctx.Done():
 			return nil
 		case run = <-g.sleep:
+			if g.rot.orientation == portrait {
+				img = image.NewGray(image.Rect(0, 0, Height, Width))
+			} else {
+				img = image.NewGray(image.Rect(0, 0, Width, Height))
+			}
+			draw.Draw(img, img.Bounds(), image.Black, image.Point{}, draw.Src)
+			label := "Waiting for reMarkable server at " + g.conf.ServerAddr
+			fnt, err := truetype.Parse(goregular.TTF)
+			if err != nil {
+				return err
+			}
+			face := truetype.NewFace(fnt, &truetype.Options{
+				Size: 36,
+			})
+			// img is now an *image.RGBA
+			point := fixed.Point26_6{
+				X: fixed.Int26_6(x * 64),
+				Y: fixed.Int26_6(y * 64),
+			}
+
+			d = &font.Drawer{
+				Dst:  img,
+				Src:  image.NewUniform(color.White),
+				Face: face, //basicfont.Face7x13,
+				Dot:  point,
+			}
+			d.DrawString(label)
+			col = color.White
+			d.Face = truetype.NewFace(fnt, &truetype.Options{
+				Size: 64,
+			})
 		case <-tick.C:
 			if run {
 				switch col {
@@ -74,11 +76,11 @@ func (g *Grabber) setWaitingPicture(ctx context.Context) error {
 				}
 				d.DrawString("X")
 				var b bytes.Buffer
-				err = jpeg.Encode(&b, img, nil)
+				err := jpeg.Encode(&b, img, nil)
 				if err != nil {
 					return err
 				}
-				err := g.displayer.Display(img)
+				err = g.displayer.Display(img)
 				if err != nil {
 					return err
 				}
