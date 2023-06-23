8dde42ddf2b36553d6cb6c066912725762f3203c
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2023-06-23 10:05:01 +0200

    feat: basic auth and TLS
    Former-commit-id: ee905bf9928f184f10a0478dab97a25f77ce0401
    Former-commit-id: 28ab4d450834794ae3daa1ffc7f429be98f5704d

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"bufio"
 	"bytes"
-	"context"
+	"crypto/tls"
+	"embed"
+	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -14,12 +17,16 @@ import (
 
 	_ "embed"
 
-	"github.com/sethvargo/go-envconfig"
+	"github.com/kelseyhightower/envconfig"
 	"nhooyr.io/websocket"
 )
 
 type configuration struct {
-	BindAddr string `env:"RK_SERVER_BIND_ADDR,default=:2001"`
+	BindAddr string `envconfig:"SERVER_BIND_ADDR" default:":2001" required:"true" description:"The server bind address"`
+	Dev      bool   `envconfig:"SERVER_DEV" default:"false" description:"Development mode: serves a local picture"`
+	Username string `envconfig:"SERVER_USERNAME" default:"admin"`
+	Password string `envconfig:"SERVER_PASSWORD" default:"password"`
+	TLS      bool   `envconfig:"HTTPS" default:"true"`
 }
 
 const (
@@ -27,45 +34,97 @@ const (
 	ScreenWidth = 1872
 	// ScreenHeight of the remarkable 2
 	ScreenHeight = 1404
+	ConfigPrefix = "RK"
 )
 
 var (
 	pointerAddr int64
 	file        io.ReaderAt
+	// Define the username and password for authentication
+	c configuration
+
+	//go:embed index.html
+	index []byte
+	//go:embed cert.pem key.pem
+	tlsAssets embed.FS
 )
 
-//go:embed index.html
-var index []byte
-
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-	defer cancel()
-	var c configuration
-	if err := envconfig.Process(ctx, &c); err != nil {
-		log.Fatal(err)
+	help := flag.Bool("h", false, "print usage")
+	flag.Parse()
+	if *help {
+		envconfig.Usage(ConfigPrefix, &c)
+		return
 	}
-	pid := findPid()
-	if len(os.Args) == 2 {
-		pid = os.Args[1]
+	if err := envconfig.Process(ConfigPrefix, &c); err != nil {
+		envconfig.Usage(ConfigPrefix, &c)
+		log.Fatal(err)
 	}
 
 	var err error
-	file, err = os.OpenFile("/proc/"+pid+"/mem", os.O_RDONLY, os.ModeDevice)
-	if err != nil {
-		log.Fatal("cannot open file: ", err)
+	if c.Dev {
+		file, err = os.OpenFile("testdata/empty.raw", os.O_RDONLY, os.ModeDevice)
+		if err != nil {
+			log.Fatal("cannot open file: ", err)
+		}
+		pointerAddr = 0
+	} else {
+		pid := findPid()
+		if len(os.Args) == 2 {
+			pid = os.Args[1]
+		}
+		file, err = os.OpenFile("/proc/"+pid+"/mem", os.O_RDONLY, os.ModeDevice)
+		if err != nil {
+			log.Fatal("cannot open file: ", err)
+		}
+		pointerAddr, err = getPointer(pid)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
-	pointerAddr, err = getPointer(pid)
-	if err != nil {
-		log.Fatal(err)
-	}
-	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
 		io.Copy(w, bytes.NewReader(index))
 	})
-	http.HandleFunc("/ws", handleWebSocket)
-	err = http.ListenAndServe(c.BindAddr, nil)
-	if err != nil {
-		log.Fatal("Error starting server:", err)
+	mux.HandleFunc("/ws", handleWebSocket)
+	if c.TLS {
+		// Load the certificate and key from embedded files
+		cert, err := tlsAssets.ReadFile("cert.pem")
+		if err != nil {
+			log.Fatal("Error reading embedded certificate:", err)
+		}
+
+		key, err := tlsAssets.ReadFile("key.pem")
+		if err != nil {
+			log.Fatal("Error reading embedded key:", err)
+		}
+
+		certPair, err := tls.X509KeyPair(cert, key)
+		if err != nil {
+			log.Fatal("Error creating X509 key pair:", err)
+		}
+
+		config := &tls.Config{
+			Certificates: []tls.Certificate{certPair},
+		}
+
+		// Create the server
+		server := &http.Server{
+			Addr:      c.BindAddr,
+			TLSConfig: config,
+			Handler:   BasicAuthMiddleware(mux), // Set the router as the handler
+
+		}
+
+		// Start the server
+		err = server.ListenAndServeTLS("", "")
+		if err != nil {
+			log.Fatal("HTTP server error:", err)
+		}
 	}
+	log.Fatal(http.ListenAndServe(c.BindAddr, BasicAuthMiddleware(mux)))
 
 }
 
@@ -93,6 +152,15 @@ func getPointer(pid string) (int64, error) {
 }
 
 func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	// Check if the request is authenticated
+	user, pass, ok := r.BasicAuth()
+	if !ok || user != c.Username || pass != c.Password {
+		// Authentication failed, send a 401 Unauthorized response
+		w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
+		w.WriteHeader(http.StatusUnauthorized)
+		fmt.Fprintln(w, "Unauthorized")
+		return
+	}
 
 	conn, err := websocket.Accept(w, r, &websocket.AcceptOptions{
 		CompressionMode: websocket.CompressionContextTakeover,
@@ -127,4 +195,29 @@ func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 		}
 		time.Sleep(200 * time.Millisecond)
 	}
+}
+
+// BasicAuthMiddleware is a middleware function that adds basic authentication to a handler
+func BasicAuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Check if the request is authenticated
+		user, pass, ok := r.BasicAuth()
+		if !ok || !checkCredentials(user, pass) {
+			// Authentication failed, send a 401 Unauthorized response
+			w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
+			w.WriteHeader(http.StatusUnauthorized)
+			fmt.Fprintln(w, "Unauthorized")
+			return
+		}
+
+		// Authentication succeeded, call the next handler
+		next.ServeHTTP(w, r)
+	})
+}
+
+// checkCredentials is a dummy function to validate the username and password
+func checkCredentials(username, password string) bool {
+	// Add your custom logic here to validate the credentials against your storage (e.g., database, file)
+	// This is a basic example, so we're using hard-coded credentials for demonstration purposes.
+	return username == "admin" && password == "password"
 }
