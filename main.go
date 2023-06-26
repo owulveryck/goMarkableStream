b80e8b55a9313e3f31eb5ddbda8c69e6d595d3eb
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2023-06-26 17:44:42 +0200

    chore: little typo
    Former-commit-id: ed0a21d93fc7b605165246a18dccb4a53145b9e5
    Former-commit-id: 45dc87267dfd827dd01dc7c66e02fea9f893c4ea

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ const (
 	ScreenWidth = 1872
 	// ScreenHeight of the remarkable 2
 	ScreenHeight = 1404
-	// ConfigPRefix for environment variable based configuration
+	// ConfigPrefix for environment variable based configuration
 	ConfigPrefix = "RK"
 )
 
