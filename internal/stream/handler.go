609bd1c02826df44ce2c7fddfbd2b3d5270a1178
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2023-07-10 15:41:36 +0200

    feat(wip): spare bandwidth and CPU

--- internal/stream/handler.go
+++ internal/stream/handler.go
@@ -36,11 +36,19 @@ type StreamHandler struct {
 	waitingQueue chan struct{}
 	file         io.ReaderAt
 	pointerAddr  int64
+	eventLoop    *remarkable.EventScanner
 }
 
 func (h *StreamHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	select {
 	case h.waitingQueue <- struct{}{}:
+		if h.eventLoop == nil {
+			h.eventLoop = remarkable.NewEventScanner()
+			defer func() {
+				h.eventLoop = nil
+			}()
+			h.eventLoop.Start(r.Context())
+		}
 		defer func() {
 			<-h.waitingQueue
 		}()
@@ -54,17 +62,27 @@ func (h *StreamHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		defer imagePool.Put(imageData) // Return the slice to the pool when done
 		// the informations are int4, therefore store it in a uint8array to reduce data transfer
 		rleWriter := rle.NewRLE(w)
+		writing := true
+		stopWriting := time.NewTicker(1 * time.Second)
+		defer stopWriting.Stop()
 
 		for {
 			select {
 			case <-ctx.Done():
 				return
+			case <-h.eventLoop.EventC:
+				writing = true
+				stopWriting.Reset(1 * time.Second)
+			case <-stopWriting.C:
+				writing = false
 			case <-h.ticker.C:
-				_, err := h.file.ReadAt(imageData, h.pointerAddr)
-				if err != nil {
-					log.Fatal(err)
+				if writing {
+					_, err := h.file.ReadAt(imageData, h.pointerAddr)
+					if err != nil {
+						log.Fatal(err)
+					}
+					rleWriter.Write(imageData)
 				}
-				rleWriter.Write(imageData)
 			}
 		}
 	default:
