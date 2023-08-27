d51ff8a5abeff35a288cb859733093c80b3aac98
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2023-08-27 10:46:35 +0200

    feat(wip): gotcha little picture

--- exploration/main.go
+++ exploration/main.go
@@ -10,17 +10,21 @@ import (
 )
 
 func main() {
-	f, err := os.Open("../testdata/full_memory_region.raw")
+	testdata := "../testdata/full_memory_region.raw"
+	stats, _ := os.Stat(testdata)
+	f, err := os.Open(testdata)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
-	backend := make([]uint8, remarkable.ScreenWidth*remarkable.ScreenHeight*4)
+	backend := make([]uint8, stats.Size())
+	log.Printf("backend is %v bytes", len(backend))
 	n, err := f.ReadAt(backend, 0)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Printf("read %v bytes from the file", n)
+	picture := backend[8 : 1872*1404*2+8]
 	boundaries := image.Rectangle{
 		Min: image.Point{
 			X: 0,
@@ -32,8 +36,18 @@ func main() {
 		},
 	}
 	img := image.NewGray(boundaries)
-	for i := 0; i < remarkable.ScreenHeight*remarkable.ScreenWidth*2; i += 2 {
-		img.Pix[i/2] = backend[i] << 4
-	}
+	w := remarkable.ScreenWidth
+	h := remarkable.ScreenHeight
+	unflipAndExtract(picture, img.Pix, w, h)
+
 	png.Encode(os.Stdout, img)
+}
+func unflipAndExtract(src, dst []uint8, w, h int) {
+	for y := 0; y < h; y++ {
+		for x := 0; x < w; x++ {
+			srcIndex := (y*w + x) * 2 // every second byte is useful
+			dstIndex := (h-y-1)*w + x // unflip position
+			dst[dstIndex] = src[srcIndex] << 4
+		}
+	}
 }
