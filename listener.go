1b95cc7465d946a90034f02f5031da442499a311
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2023-09-29 14:57:03 +0200

    feat(wip): ngrok feature

---
+++ listener.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"context"
+	"net"
+
+	"golang.ngrok.com/ngrok"
+	"golang.ngrok.com/ngrok/config"
+)
+
+func setupListener(ctx context.Context, s *configuration) (net.Listener, error) {
+	if s.BindAddr == "ngrok" {
+		l, err := ngrok.Listen(ctx,
+			config.HTTPEndpoint(),
+			ngrok.WithAuthtokenFromEnv(),
+		)
+		s.BindAddr = l.Addr().String()
+		return l, err
+	}
+	return net.Listen("tcp", s.BindAddr)
+}
