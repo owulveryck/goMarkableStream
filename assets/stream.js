2e0267095e7657d51a59e80ea8ce68688d99217c
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2023-10-01 10:41:19 +0200

    feat(wip): rotation

--- assets/stream.js
+++ assets/stream.js
@@ -1,21 +1,22 @@
 let rotate = true;  // start with a false boolean
 let withColor = true;  // start with a false boolean
+let recordingWithSound = false;
 document.getElementById('rotate').addEventListener('click', function() {
 	rotate  = !rotate;  // toggle the boolean
-	
-    // Toggle the 'toggled' class on the button
-    this.classList.toggle('toggled');
 
-    resizeAndCopy();
+	// Toggle the 'toggled' class on the button
+	this.classList.toggle('toggled');
+
+	resizeAndCopy();
 });
 
 document.getElementById('colors').addEventListener('click', function() {
 	withColor  = !withColor;  // toggle the boolean
-	
-    // Toggle the 'toggled' class on the button
-    this.classList.toggle('toggled');
 
-    resizeAndCopy();
+	// Toggle the 'toggled' class on the button
+	this.classList.toggle('toggled');
+
+	resizeAndCopy();
 });
 
 const sidebar = document.querySelector('.sidebar');
@@ -278,13 +279,33 @@ async function initiateStream() {
 
 let mediaRecorder;
 let recordedChunks = [];
-function startRecording() {
-	console.log("recording in progress");
-	let stream = fixedCanvas.captureStream(25); // 25 fps
+async function startRecording() {
+	const tempCanvas = createTempCanvas(); // Create the temporary canvas
 
-	mediaRecorder = new MediaRecorder(stream, {
-		mimeType: 'video/webm;codecs=vp9'
-	});
+	console.log("recording in progress");
+	let videoStream = tempCanvas.captureStream(25); // 25 fps
+
+	if (recordingWithSound) {
+		// Capture audio stream from the user's microphone
+		let audioStream;
+		try {
+			audioStream = await navigator.mediaDevices.getUserMedia({ audio: true });
+		} catch (err) {
+			console.error("Error capturing audio:", err);
+			return;
+		}
+
+		// Combine video and audio streams
+		let combinedStream = new MediaStream([...videoStream.getTracks(), ...audioStream.getTracks()]);
+
+		mediaRecorder = new MediaRecorder(combinedStream, {
+			mimeType: 'video/webm;codecs=vp9'
+		});
+	} else {
+		mediaRecorder = new MediaRecorder(videoStream, {
+			mimeType: 'video/webm;codecs=vp9'
+		});
+	}
 
 	mediaRecorder.ondataavailable = function(event) {
 		if (event.data.size > 0) {
@@ -301,6 +322,12 @@ function startRecording() {
 
 function stopRecording() {
 	mediaRecorder.stop();
+	removeTempCanvas(); // Remove the temporary canvas after recording
+
+	// Stop updating tempCanvas
+	if (animationFrameId) {
+		cancelAnimationFrame(animationFrameId);
+	}
 }
 
 function download() {
@@ -321,6 +348,23 @@ function download() {
 	}, 100);
 }
 
+document.getElementById('startStopButtonWithSound').addEventListener('click', function() {
+	let icon = document.getElementById('icon2');
+	let label = document.getElementById('label2');
+
+
+	if (label.textContent === 'Record with audio') {
+		label.textContent = 'Stop';
+		icon.classList.add('recording');
+		recordingWithSound = true;
+		startRecording();
+	} else {
+		label.textContent = 'Record with audio';
+		icon.classList.remove('recording');
+		recordingWithSound = false;
+		stopRecording();
+	}
+});
 document.getElementById('startStopButton').addEventListener('click', function() {
 	let icon = document.getElementById('icon');
 	let label = document.getElementById('label');
@@ -336,6 +380,67 @@ document.getElementById('startStopButton').addEventListener('click', function() 
 		stopRecording();
 	}
 });
+function createTempCanvas() {
+	const tempCanvas = document.createElement('canvas');
+	tempCanvas.width = fixedCanvas.width;
+	tempCanvas.height = fixedCanvas.height;
+	tempCanvas.id = 'tempCanvas'; // Assign an ID for easy reference
+
+	// Hide the tempCanvas
+	tempCanvas.style.display = 'none';
+
+	// Start updating tempCanvas
+	updateTempCanvas(tempCanvas);
+
+	// Append tempCanvas to the body (or any other container)
+	document.body.appendChild(tempCanvas);
+
+	return tempCanvas;
+}
+function removeTempCanvas() {
+	const tempCanvas = document.getElementById('tempCanvas');
+	if (tempCanvas) {
+		tempCanvas.remove();
+	}
+}
+let animationFrameId;
+
+function updateTempCanvas(tempCanvas) {
+	const tempContext= tempCanvas.getContext('2d');
+	if (rotate) {
+		// clear the canvas
+		tempContext.clearRect(0, 0, tempCanvas.width, tempCanvas.height);
+		tempContext.save(); // Save the current state
+		// Calculate the destination coordinates for drawing the rotated image
+		var destX = (tempCanvas.width - fixedCanvas.height) / 2;
+		var destY = (tempCanvas.height - fixedCanvas.width) / 2;
+		var destWidth = fixedCanvas.height;
+		var destHeight = fixedCanvas.width;
+
+		// Move the rotation point to the center of the rectangle
+		tempContext.translate(tempCanvas.width / 2, tempCanvas.height / 2);
+
+		// Rotate the canvas
+		tempContext.rotate(Math.PI / 180 * 270); // Rotate 45 degrees
+
+		tempContext.translate(-tempCanvas.width / 2, -tempCanvas.height / 2);
+
+		// Draw the image on the second canvas
+		//tempContext.drawImage(fixedCanvas, -fixedCanvas.width / 2, -fixedCanvas.height / 2);
+		//					tempContext.drawImage(fixedCanvas, 0,0, tempCanvas.width, tempCanvas.height);
+		tempContext.drawImage(fixedCanvas, 0, 0, fixedCanvas.width, fixedCanvas.height, destX, destY, destWidth, destHeight);
+
+		tempContext.restore(); // Restore the state
+	} else {
+		tempContext.drawImage(fixedCanvas, 0, 0, tempCanvas.width, tempCanvas.height);
+	}
+	//tempContext.drawImage(fixedCanvas, 0, 0);
+
+	// Continue updating tempCanvas
+	animationFrameId = requestAnimationFrame(() => updateTempCanvas(tempCanvas));
+
+}
 initiateStream();
+
 
 
