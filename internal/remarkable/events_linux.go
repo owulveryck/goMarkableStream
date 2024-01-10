3e80bd0a88dff3ede4799e59e9ad69a6812d7086
Moritz Poldrack <git@moritz.sh>
2024-01-10 10:35:27 +0100

    feat: remove panic when failing to acquire filehandles
    Signed-off-by: Moritz Poldrack <git@moritz.sh>

--- internal/remarkable/events_linux.go
+++ internal/remarkable/events_linux.go
@@ -20,13 +20,13 @@ type EventScanner struct {
 
 // NewEventScanner ...
 func NewEventScanner() *EventScanner {
-	pen, err := os.OpenFile("/dev/input/event1", os.O_RDONLY, 0644)
+	pen, err := os.OpenFile("/dev/input/event1", os.O_RDONLY, 0o644)
 	if err != nil {
-		panic(err)
+		log.Fatalf("failed to read pen position: %v", err)
 	}
-	touch, err := os.OpenFile("/dev/input/event2", os.O_RDONLY, 0644)
+	touch, err := os.OpenFile("/dev/input/event2", os.O_RDONLY, 0o644)
 	if err != nil {
-		panic(err)
+		log.Fatalf("failed to read touch position: %v", err)
 	}
 	return &EventScanner{
 		pen:   pen,
