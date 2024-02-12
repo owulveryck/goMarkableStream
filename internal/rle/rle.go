a6c465028c2de4818b9b935a129cbc90508e550e
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2024-02-12 17:27:20 +0100

    feat: more optimization

--- internal/rle/rle.go
+++ internal/rle/rle.go
@@ -50,7 +50,7 @@ func (rlewriter *RLE) Write(data []byte) (int, error) {
 	current := data[0]
 	count := uint8(0)
 
-	for i := 0; i < len(data); i += 2 {
+	for i := 0; i < remarkable.ScreenHeight*remarkable.ScreenWidth*2; i += 2 {
 		datum := data[i]
 		if count < 254 && datum == current {
 			count++
