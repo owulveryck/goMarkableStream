45c8d1a0243bf9566d9d18088fc84429d4aa9e2f
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2024-03-09 12:09:57 +0100

    test

--- listener.go
+++ listener.go
@@ -6,10 +6,20 @@ import (
 
 	"golang.ngrok.com/ngrok"
 	"golang.ngrok.com/ngrok/config"
+	"tailscale.com/tsnet"
 )
 
 func setupListener(ctx context.Context, s *configuration) (net.Listener, error) {
-	if s.BindAddr == "ngrok" {
+	switch s.BindAddr {
+	case "tailscale":
+		srv := new(tsnet.Server)
+		srv.Hostname = "gomarkablestream"
+		if !s.DevMode {
+			srv.Logf = func(string, ...any) {}
+		}
+		// Disable logs
+		return srv.Listen("tcp", ":2001")
+	case "ngrok":
 		l, err := ngrok.Listen(ctx,
 			config.HTTPEndpoint(),
 			ngrok.WithAuthtokenFromEnv(),
@@ -17,6 +27,7 @@ func setupListener(ctx context.Context, s *configuration) (net.Listener, error) 
 		s.BindAddr = l.Addr().String()
 		c.TLS = false
 		return l, err
+	default:
+		return net.Listen("tcp", s.BindAddr)
 	}
-	return net.Listen("tcp", s.BindAddr)
 }
