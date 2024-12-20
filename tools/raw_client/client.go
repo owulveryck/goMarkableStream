4443acd9e75c4c4b8fd21c4512c865ae2fef0dd3
Alexander Akhmetov <me@alx.cx>
2024-12-20 18:12:24 +0100

    Add README.md to tools/raw_client

--- tools/raw_client/client.go
+++ tools/raw_client/client.go
@@ -11,18 +11,6 @@ import (
 	"os"
 )
 
-/*
-This tool connects to a server over HTTPS, fetches raw RGBA image data,
-and saves it as a PNG file. It supports basic authentication and allows
-configuration of server details, image dimensions, and output file.
-
-Run the server on your reMarkable:
-   RK_DEV_MODE=true ./goMarkableStream
-
-Run the client:
-   go run tools/client/client.go --ip 10.0.0.1  # Replace with your device's IP
-*/
-
 func downloadRawImage(ip, port, username, password string) ([]byte, error) {
 	url := fmt.Sprintf("https://%s:%s/raw", ip, port)
 	client := &http.Client{
